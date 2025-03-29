d314c481fcffbba9362164d3362af22dc8ff71a7
DarrelNugget <darrelnguyen03@gmail.com>
2025-03-29 14:23:07 -0600

    Initial commit

---
+++ SeasonTwo-TheBasics/Episode-3-Variables&Datatypes.py
@@ -0,0 +1,11 @@
+character_name = "Darrel"
+character_age = "22"
+character_secondAge = "22.23231321"
+is_male = True
+
+print("There once was a men named " + character_name + ",")
+print("he was " + character_age + " years old")
+
+character_name = "Bob"
+print("There once was a men named " + character_name + ",")
+print("he was " + character_age + " years old")
